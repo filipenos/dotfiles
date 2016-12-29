b3efe18d4082aea26580fe102b0ed6cb928be795
Filipe Peixoto <filipe.peixoto@beneficiofacil.com.br>
2016-12-29 17:26:57 -0200

    Verify if sdk already installed

--- bin/gaesdk.sh
+++ bin/gaesdk.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+log() {
+	echo "[gaesdk] $@"
+}
+
+VERSION="1.9.48"
+
 if [ ! -d "$HOME/lib" ]; then
 	mkdir $HOME/lib
 fi
 
 if [ -d "$HOME/lib/go_appengine" ]; then
-	echo "gae sdk already installed, abort..."
-	head -n 1 "$HOME/lib/go_appengine/VERSION"
-	exit 0
+	log "gae sdk already installed, abort..."
+	log $(head -n 1 "$HOME/lib/go_appengine/VERSION")
+else
+	log "downloading $VERSION"
+	wget "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-$VERSION.zip" -O /tmp/appengine_sdk.zip
+	log "unpacking sdk"
+	unzip -q -d $HOME/lib /tmp/appengine_sdk.zip
+	log "installation done"
 fi
 
-wget https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.48.zip -O /tmp/appengine_sdk.zip
-unzip -d $HOME/lib /tmp/appengine_sdk.zip
 
-echo 'export PATH="$PATH:$HOME/lib/go_appengine"' >> $HOME/.customrc
+
+if grep -q -1 "go_appeninge" $HOME/.customrc ; then
+	log "configuring .bashrc"
+	echo 'export PATH="$PATH:$HOME/lib/go_appengine"' >> $HOME/.customrc
+else
+	log "skip .customrc configuration"
+fi
+
+exit 0
