7d988043e22e2860e5d53dcb39a2a0afcceda9a8
Filipe Peixoto <filipe.peixoto@beneficiofacil.com.br>
2016-12-29 17:27:54 -0200

    Create script to install default configurations of home

---
+++ install.sh
@@ -0,0 +1,46 @@
+#/bin/bash
+
+log() {
+	echo "[DOTFILES] $@"
+}
+
+install() {
+	log "installing dotfiles on $HOME"
+	git clone -q https://git@github.com/filipenos/dotfiles "$HOME/dotfiles"
+	log "dotfiles installed on $HOME"
+}
+
+install_customrc() {
+	echo "configuring customrc"
+	$HOME/dotfiles/bin/custom-path.sh -c
+}
+
+install_vim() {
+	log "configuring vimrc"
+	$HOME/dotfiles/bin/vim-install-vundle
+}
+
+install_tmux() {
+	log "configuring tmux.conf"
+	ln -s $HOME/dotfiles/etc/tmux.conf $HOME/.tmux.conf
+}
+
+remove() {
+	log "removing dotfiles from $HOME"
+	rm -rf $HOME/dotfiles
+}
+
+if [ -d "$HOME/dotfiles" ]; then
+	log "dotfiles already installed, updating..."
+	git -C "$HOME/dotfiles" pull
+else
+	install
+	install_vim
+	install_customrc	
+
+	log "adding dotfiles to path"
+	echo "export PATH=$PATH:$HOME/dotfiles/bin" >> $HOME/.customrc
+fi
+
+log "installation occurred successfully! have a good time"
+exit 0
