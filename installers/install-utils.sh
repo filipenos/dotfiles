b29802a90563e9b5a06b229c03f2390527c0264a
Filipe Peixoto <filipenos@gmail.com>
2017-01-11 16:08:14 -0200

    Add command to install funcoeszz

--- installers/install-utils.sh
+++ installers/install-utils.sh
@@ -11,12 +11,22 @@ case "$1" in
 		log "installation done"
 		exit 0
 		;;
+
 	tpm)
 		log "installing tmux plugin manager (tpm)"
 		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 		log "installation done"
 		;;
+
+  funcoeszz)
+    log "installing funcoeszz"
+    wget "http://funcoeszz.net/download/funcoeszz-15.5.sh" -O "$HOME/bin/funcoeszz-15.5.sh"
+    chmod +x "$HOME/bin/funcoeszz-15.5.sh"
+    log "installation done"
+    ;;
+
 	*)
 		echo "install custom resource"
+    echo "    {ohmysql|tpm|funcoeszz}"
 		;;
 esac
