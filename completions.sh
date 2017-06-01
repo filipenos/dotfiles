74a73e11e3994233a970558339ea587645b56d26
Filipe Peixoto <filipe.peixoto@beneficiofacil.com.br>
2017-06-01 17:31:59 -0300

    Improvements on complete functions

--- completions.sh
+++ completions.sh
@@ -3,13 +3,20 @@ _go()
   local cur=${COMP_WORDS[COMP_CWORD]}
   COMPREPLY=( $(compgen -W "build clean doc env fix fmt generate get install list run test tool version vet" -- $cur) )
 }
-complete -F _go go
-complete -F _go goapp
+complete -F _go go goapp
 
 _aeremote()
 {
-  local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "-host -port -dump -load -pretty -batch-size -debug" -- $cur) )
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="-host -port -dump -load -pretty -batch-size -debug"
+
+  if [[ ${prev} == -*load ]]; then
+    COMPREPLY=( $(compgen -f -- ${cur}) )
+  else
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  fi
 }
-complete -F _aeremote aeremote
-complete -F _aeremote aeremote.sh
+complete -F _aeremote aeremote aeremote.sh
