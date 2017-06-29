c9c8869bf82e9e102bb507dee589fc8346b3d00b
Filipe Peixoto <filipe.peixoto@beneficiofacil.com.br>
2017-06-29 11:42:23 -0300

    Improvements on go completions

--- completions.sh
+++ completions.sh
@@ -1,7 +1,18 @@
-_go()
-{
-  local cur=${COMP_WORDS[COMP_CWORD]}
-  COMPREPLY=( $(compgen -W "build clean doc env fix fmt generate get install list run test tool version vet" -- $cur) )
+_go() {
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  case "${COMP_WORDS[COMP_CWORD-1]}" in
+    "go")
+      comms="build clean doc env fix fmt generate get install list run test tool version vet"
+      COMPREPLY=($(compgen -W "${comms}" -- ${cur}))
+      ;;
+    *)
+      files=$(find ${PWD} -mindepth 1 -maxdepth 1 -type f -iname "*.go" -exec basename {} \;)
+      dirs=$(find ${PWD} -mindepth 1 -maxdepth 1 -type d -not -name ".*" -exec basename {} \;)
+      repl="${files} ${dirs}"
+      COMPREPLY=($(compgen -W "${repl}" -- ${cur}))
+      ;;
+  esac
+  return 0
 }
 complete -F _go go goapp
 
