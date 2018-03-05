ff77c374dfc32db3f2b71e586a0ac1c03a7f5351
Filipe Peixoto <filipenos@gmail.com>
2018-03-05 08:40:20 -0300

    Allow to copy/paste to middle button of mouse

--- bin/copy.sh
+++ bin/copy.sh
@@ -1,5 +1,30 @@
-#/bin/sh
+#!/bin/sh
 
 xclip -h >/dev/null 2>&1 || sudo apt-get install xclip
 
-exec xclip -selection clipboard "$@"
+SELECTION="clipboard"
+
+help() {
+  echo "Copy selections of shell"
+  echo "  Usage $0 [options]"
+  echo "    -c|--clip|--clipboard to clipboard (default)"
+  echo "    -m|--middle to primary"
+  echo "    -h|--help to print this message"
+}
+
+case "$1" in
+  -c|--clip|--clipboard)
+    shift
+    SELECTION="clipboard"
+    ;;
+  -m|--middle)
+    shift
+    SELECTION="primary"
+    ;;
+  -h|--help)
+    help
+    return 0
+    ;;
+esac
+
+exec xclip -selection $SELECTION -i "$@"
--- bin/paste.sh
+++ bin/paste.sh
@@ -2,4 +2,29 @@
 
 xclip -h >/dev/null 2>&1 || sudo apt-get install xclip
 
-exec xclip -selection clipboard -o "$@"
+SELECTION="clipboard"
+
+help() {
+  echo "Paste selections on shell"
+  echo "  Usage $0 [options]"
+  echo "    -c|--clip|--clipboard to clipboard (default)"
+  echo "    -m|--middle to primary"
+  echo "    -h|--help to print this message"
+}
+
+case "$1" in
+  -c|--clip|--clipboard)
+    shift
+    SELECTION="clipboard"
+    ;;
+  -m|--middle)
+    shift
+    SELECTION="primary"
+    ;;
+  -h|--help)
+    help
+    return 0
+    ;;
+esac
+
+exec xclip -selection $SELECTION -o "$@"
