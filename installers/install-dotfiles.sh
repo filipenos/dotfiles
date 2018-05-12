a62d74ba74ebe6379841631ad9799ea9acb7c42a
Filipe Peixoto <filipenos@gmail.com>
2018-05-12 10:21:16 -0300

    Improvements on install of dotfiles

--- installers/install-dotfiles.sh
+++ installers/install-dotfiles.sh
@@ -9,6 +9,29 @@ install() {
 	git clone -q https://git@github.com/filipenos/dotfiles "$HOME/dotfiles"
 }
 
+configure_onpath() {
+	log "configure dotfiles on bashrc"
+	if ! grep -q "#DOTFILESRC" $HOME/.bashrc ; then
+		echo 'source $HOME/dotfiles/dotfilesrc #DOTFILESRC' >> $HOME/.bashrc
+	else
+		log "bashrc already configured"
+	fi
+	log "configure dotfiles on zshrc"
+	if ! grep -q "#DOTFILESRC" $HOME/.zshrc ; then
+		echo 'source $HOME/dotfiles/dotfilesrc #DOTFILESRC' >> $HOME/.zshrc
+	else
+		log "zshrc already configured"
+	fi
+	log "reload your sh shell to use dotfiles"
+}
+
+remove_frompath() {
+	log "remove dotfilesrc from .bashrc"
+	sed -i '/DOTFILESRC/d' "$HOME/.bashrc"
+	log "remove dotfilesrc from .zshrc"
+	sed -i '/DOTFILESRC/d' "$HOME/.zshrc"
+}
+
 install_vim() {
 	log "configuring vimrc"
 	$HOME/dotfiles/bin/vim-install-vundle
@@ -32,6 +55,8 @@ else
 	install_vim
 	install_tmux
 fi
+configure_onpath
+#remove_frompath
 
 log "installation occurred successfully! have a good time"
 exit 0
