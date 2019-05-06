b4d018c4593124ffe807276c7b94cb9e2817d350
Filipe Peixoto <filipenos@gmail.com>
2019-05-06 16:13:31 -0300

    Fixes repeat script

--- bin/repeat.sh
+++ bin/repeat.sh
@@ -1,39 +1,11 @@
 #!/bin/bash
 
-# TODO Add suport to params
-# rename repeat.sh to repeat
-# -quant quant of repeats, default is infinite
-# -clear if clear after run command
-
-quant=
-clean=
-
-while [ "$1" != "" ]; do
-	case $1 in
-		-q | --quant)
-      shift
-      quant=$1
-      ;;
-		-c | --clean)
-      shift
-      clean=$1
-      ;;
-    -h | --help)
-      help
-      ;;
-  esac
-done
-
 if [ -z "$1" ]
 then
 	echo "command is required!"
 	exit 1
 fi
 
-echo "exucuting ${*:1}"
-
-while true
-do
-	${*:1}
-	sleep 1
+for d in $(ls -1); do
+  eval $@ $d
 done
