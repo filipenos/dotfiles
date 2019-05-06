cecc8487b00f2302b9dae5fce4592cdb8b1dcf48
Filipe Peixoto <filipenos@gmail.com>
2019-05-05 21:21:33 -0300

    Check mimetype of file, only directory are permited
    Also check if parent directory has added
    Fixes minor bugs found

--- bin/save-my-pc.sh
+++ bin/save-my-pc.sh
@@ -2,11 +2,11 @@
 
 #TODO (filipenos) permitir adicionar arquivo
 
-#TODO (filipenos) verificar se um diretório já esta sendo sincronizado com o pai
-#por exemplo, existe /home/filipe/Documentos, se tentar adicionar /home/filipe/Documentos/Planilhas deve retornar erro pois
-#o diretorio acima já esta sendo salvo
-
 #TODO (filipenos) permitir salvar na amazon s3, e falar o nome do bucket, talvez com um arquivo de configuração
+
+#TODO (filipenos) permitir não apagar os arquivos
+
+#TODO (filipenos) permitir apagar um diretório que foi removido
 
 
 FILE_WITH_PATHS=$HOME/.savemypc
@@ -23,40 +23,52 @@ print_help() {
 }
 
 is_empty() {
-  if [ -z $1 ]; then
+  if [ -z "$1" ]; then
     log "[ERROR] empty"
     exit 1
   fi
 }
 
 check_file_exists() {
-  is_empty $1
-  if [ ! -f $1 ]; then
+  is_empty "$1"
+  if [ ! -f "$1" ]; then
     log "[ERROR] File $1 not exists"
     exit 1
   fi
 }
 
 check_path_exists() {
-  is_empty $1
-  if [ ! -d $1 ]; then
+  is_empty "$1"
+  if [ ! -d "$1" ]; then
     log "[ERROR] Path $1 not exists"
     exit 1
   fi
 }
 
 add_path() {
-  log "Add $1 to save"
-  check_path_exists $1
-  typ=$(mimetype --output-format "%m" $1)
+  to_add=$(realpath "$1")
+  #to_add="$1"
+  log "Add $to_add to save"
+  check_path_exists "$to_add"
+  typ=$(mimetype --output-format "%m" "$to_add")
   if [ $typ != "inode/directory" ]; then
     log "[ERROR] Current type $typ is not supported"
     exit 1
   fi
 
-  dir=$(dirname "${1}")
-  file="$dir/$file"$(basename "${1}")
-  grep -q -s "$file" $FILE_WITH_PATHS
+  p=$(dirname "$to_add")
+  while [ "$p" != "/" ]; do
+    grep -q -s -e "$p\$" $FILE_WITH_PATHS
+    if [ $? -eq 0 ]; then
+      log "[ERROR] parent directory already added"
+      exit 1
+    fi
+    p=$(dirname $p)
+  done
+
+  dir=$(dirname "$to_add")
+  file="$dir/"$(basename "$to_add")
+  grep -q -s -e "$file\$" $FILE_WITH_PATHS
   if [ $? -gt 0 ]; then
     log "Path added successfully!"
     echo $file >> $FILE_WITH_PATHS
@@ -85,10 +97,6 @@ check_dependencies() {
   fi
 }
 
-mime_type() {
-  mimetype --output-format "%m" examples.desktop
-}
-
 save_my_pc() {
   check_dependencies
   check_file_exists $FILE_WITH_PATHS
@@ -96,10 +104,10 @@ save_my_pc() {
   log "Saving pc data to $PC_NAME"
   while read l;
   do
-    dir=$(basename $l)
-    log "Saving path $dir"
     check_path_exists "$l"
-    gsutil -m rsync -d -r "$l" "gs://$BUCKET_NAME/$pcname/$dir"
+    dir=$(echo "$l" | sed 's,'"$HOME/"',,g')
+    log "Saving path $dir"
+    gsutil -m rsync -d -r "$l" "gs://$BUCKET_NAME/$PC_NAME/$dir"
   done < $FILE_WITH_PATHS
 }
 
@@ -111,7 +119,7 @@ while [ $# -gt 0 ]; do
   case "$1" in
     add)
       shift
-      add_path $1
+      add_path "$1"
       ;;
     show)
       show_paths
