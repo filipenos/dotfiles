f1530eeb37c4f41511b15d34ace1e6c80a391c81
Filipe Peixoto <filipenos@gmail.com>
2019-07-25 14:21:39 -0300

    Improvements on script to run/build go files

--- bin/go-on-container.sh
+++ bin/go-on-container.sh
@@ -2,10 +2,22 @@
 
 set -e
 
-path="$1"
+GOLANG_VERSION="latest"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -b|--build)
+      BUILD="1"
+      ;;
+    *)
+      path="$1"
+  esac
+  shift
+done
+
 if [ -z "$path" ]; then
-	echo "using pwd $(pwd)"
-	path=$(pwd)
+  echo "using pwd $(pwd)"
+  path=$(pwd)
 fi
 
 path=$(realpath "$path")
@@ -14,9 +26,22 @@ project=$(echo "$path" | sed -e "s;^$GOPATH/;;")
 
 echo "running $name > $project > $path on container"
 
-docker run \
-	--rm \
-	--name "$name" \
-	-v "$path":"/go/$project" \
-	-w "/go/$project" \
-	-it golang:latest
+if [ ! -z $BUILD ]; then
+  echo "building..."
+  docker run \
+    --rm \
+    --name "$name" \
+    -v "$path":"/go/$project" \
+    -w "/go/$project" \
+    -it "golang:$GOLANG_VERSION" \
+    go build
+else
+  docker run \
+    --rm \
+    --name "$name" \
+    -v "$path":"/go/$project" \
+    -w "/go/$project" \
+    -it "golang:$GOLANG_VERSION"
+fi
+
+
