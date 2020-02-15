3012bee8b05655355ce7fbbdc5671c782947ad75
Filipe Peixoto <filipenos@gmail.com>
2020-02-15 13:34:59 -0300

    Update my extensions list
    Also add script to backup/load my_extensions

---
+++ vscode/extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$1" == "install" ]; then
+  echo "install my extensions"
+  while IFS= read -r ext
+  do
+    code --install-extension $ext
+  done < "my_extensions"
+else
+  echo "save my extensions"
+  code --list-extensions > my_extensions
+  cat my_extensions
+fi
