3108481e56cd44ae768dad4b8c7bfceb76ea7dff
Filipe Peixoto <filipenos@gmail.com>
2020-02-24 22:07:01 -0300

    Allow run coder from anywhere hos

--- containers/run-coder.sh
+++ containers/run-coder.sh
@@ -2,7 +2,7 @@ docker run \
   --rm \
   -it \
   --name "coder" \
-  -p 127.0.0.1:8080:8080 \
+  -p 8080:8080 \
   -e PASSWORD=filipe \
   -v "$PWD:/home/coder/project" \
   -v "$HOME/dotfiles/vscode/settings.json:/User/settings.json" \
