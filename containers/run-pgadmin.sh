4d89844287857fedb6bb371829660658deb5a4db
Filipe Peixoto <filipenos@gmail.com>
2020-03-28 18:31:31 -0300

    Fixes run pgadmin

--- containers/run-pgadmin.sh
+++ containers/run-pgadmin.sh
@@ -5,5 +5,5 @@ docker run \
   -p 8090:80 \
   -e "PGADMIN_DEFAULT_EMAIL=filipenos@gmail.com" \
   -e "PGADMIN_DEFAULT_PASSWORD=filipe" \
-  -v "$HOME/Code/volumes/pgadmin-data:/var/lib/pgadmin" \
-  dpage/pgadmin4
+  -v "gadmin-data:/var/lib/pgadmin" \
+  dpage/pgadmin4:latest
