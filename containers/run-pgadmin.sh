3c59af89c5eaab9843e09b86740a7eed77535787
Filipe Peixoto <filipenos@gmail.com>
2020-03-29 14:10:47 -0300

    Fix script to run pgadmin

--- containers/run-pgadmin.sh
+++ containers/run-pgadmin.sh
@@ -2,8 +2,8 @@ docker run \
   --rm \
   --detach \
   --name "run-pgadmin" \
-  -p 8090:80 \
+  --publish 8090:80 \
   -e "PGADMIN_DEFAULT_EMAIL=filipenos@gmail.com" \
   -e "PGADMIN_DEFAULT_PASSWORD=filipe" \
-  -v "gadmin-data:/var/lib/pgadmin" \
+  -v "pgadmin-data:/var/lib/pgadmin" \
   dpage/pgadmin4:latest
