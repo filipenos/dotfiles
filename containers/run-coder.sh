36b2cb8fd392977af9b719ab5314161fa58db36a
Filipe Peixoto <filipenos@gmail.com>
2020-03-29 09:18:25 -0300

    Improvements on run coder

--- containers/run-coder.sh
+++ containers/run-coder.sh
@@ -1,9 +1,34 @@
+#!/bin/bash
+
+if [ "$(docker ps -q -f name=coder)" ]; then
+  read -p "alread running container coder, stop this (yes|no): "  yesno
+  if [ "$yesno" = "yes" ]; then
+    echo "stop container"
+    docker stop coder
+  fi  
+  exit 0
+fi
+
+if [ -d "$1" ]; then
+  runpath=$(realpath $1)
+else
+  runpath=$(pwd)
+fi
+
+echo "running coder on path $runpath"
+
 docker run \
   --rm \
-  -it \
+  --interactive \
+  --tty \
+  --detach \
+  --user "$(id -u):$(id -g)" \
   --name "coder" \
-  -p 8080:8080 \
-  -e PASSWORD=filipe \
-  -v "$PWD:/home/coder/project" \
-  -v "$HOME/dotfiles/vscode/settings.json:/User/settings.json" \
+  --hostname "coder" \
+  --publish 8080:8080 \
+  --env PASSWORD=filipe \
+  --volume "/etc/passwd:/etc/passwd:ro" \
+  --volume "/home/filipe:/home/filipe" \
+  --volume "/home/linuxbrew:/home/linuxbrew" \
+  --volume "$runpath:/home/filipe/project" \
   codercom/code-server
