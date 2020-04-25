039eda4fbdd9ac750f6cc328ea2a87de12e49876
Filipe Peixoto <filipenos@gmail.com>
2020-04-24 21:13:52 -0300

    Add option to stop container

--- containers/run-portainer.sh
+++ containers/run-portainer.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
+NAME=run-portainer
+
+if [ "$(docker ps -q -f name=$NAME)" ]; then
+  read -p "alread running container $NAME, stop this (yes|no): "  yesno
+  if [ "$yesno" = "yes" ]; then
+    echo "stop container"
+    docker stop $NAME
+  fi
+  exit 0
+fi
+
 docker run \
   --rm \
   -d \
-  --name run-portainer \
+  --name $NAME \
   -p 9000:9000 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_data:/data \
