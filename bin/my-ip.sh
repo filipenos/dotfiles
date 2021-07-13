54e27261f702813185ebfb546457e81ea692b920
Filipe Peixoto <filipenos@gmail.com>
2021-07-13 10:44:27 -0300

    feats

--- bin/my-ip.sh
+++ bin/my-ip.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -s http://whatismijnip.nl |cut -d " " -f 5
+curl checkip.amazonaws.com
+#curl ifconfig.me
+#curl icanhazip.com
+#curl ipecho.net/plain
+#curl ifconfig.co
+#curl -s http://whatismijnip.nl |cut -d " " -f 5
