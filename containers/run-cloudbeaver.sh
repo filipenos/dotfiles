a8ef99c351f9c26a02c994b7883dca72aee87ee2
Filipe Peixoto <filipenos@gmail.com>
2023-08-03 15:19:32 -0300

    add internal ip to cloudbeaver container

--- containers/run-cloudbeaver.sh
+++ containers/run-cloudbeaver.sh
@@ -16,10 +16,13 @@ if [ -d "$1" ]; then
   path=$(realpath $1)
 fi
 
+export CB_LOCAL_HOST_ADDR=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)
+
 docker run \
   --rm \
   --detach \
   --name "$name" \
   --publish 8978:8978 \
+  --add-host=host.docker.internal:${CB_LOCAL_HOST_ADDR} \
   -v $HOME/.cloudbeaver/workspace:/opt/cloudbeaver/workspace \
   dbeaver/cloudbeaver:latest
