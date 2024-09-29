414b3af24fbde1a3074cb11bb5c60293f54441f5
Filipe Peixoto <filipenos@gmail.com>
2024-09-28 23:55:23 -0300

    feat: improvements on node-on-container

--- bin/node-on-container.sh
+++ bin/node-on-container.sh
@@ -5,56 +5,96 @@ set -e
 NODE_VERSION="lts"
 RUN="bash"
 
+path=$(pwd)
+path=$(realpath "$path")
+project=$(basename "$path")
+
+if [ -f package.json ]; then
+  name=$(node -p "require('./package.json').name")
+  main=$(node -p "require('./package.json').main")
+else
+  name=$project
+fi
+
 usage() {
   cat <<HELP_USAGE
-  usage $0 [-v] [-h] path
-    -v    version of node (actual: $NODE_VERSION)
-    -h    print help
+  usage $0 [-r|-m] [-v] [-h]
+  current: $name > $project > $path
+    --run (-r)
+      run npm script
+    --main (-m)
+      run main file (actual: $main)
+    --node-modules-as-volume
+      use node_modules as volume
+    --install
+      npm install before run
+    --version (-v)
+      version of node (actual: $NODE_VERSION)
+    --help (-h)
+      print help
 HELP_USAGE
   exit 1
 }
 
 while [ $# -gt 0 ]; do
   case "$1" in
-    -r|--run)
+    -r|-run|--run)
+      shift
+      RUN="npm run $1"
+      ;;
+    -m|-main|--main)
       shift
       RUN="node $1"
       ;;
-    -v|--version)
+    --node-modules-as-volume)
+      NODE_MODULES_AS_VOLUME="1"
+      ;;
+    --install)
+      NPM_INSTALL="1"
+      ;;
+    -v|-version|--version)
       shift
       NODE_VERSION="$1"
       ;;
-    -h|--help)
+    -h|-help|--help)
       usage
       ;;
-    *)
-      path="$1"
   esac
   shift
 done
-
-if [ -z "$path" ]; then
-  echo "using pwd $(pwd)"
-  path=$(pwd)
-fi
-
-path=$(realpath "$path")
-name=$(basename "$path")
-project=$name
-
-echo "running $name > $project > $path on container golang:$GOLANG_VERSION"
-
 
 PORTS=${PORTS}
 if [ -n "$PORTS" ]; then
   PORTS="-p $PORTS"
 fi
 
+if [ ! -z $NODE_MODULES_AS_VOLUME ]; then
+  echo "using node_modules as volume"
+  VOLUMES="-v node_volumes:/app/$project/node_modules"
+fi
+
+if [ ! -z $NPM_INSTALL ]; then
+  echo "npm install ..."
+  docker run \
+    --rm \
+    --init \
+    --name "$name" \
+    -v "$path":"/app/$project" \
+    $VOLUMES \
+    -w "/app/$project" \
+    -it "node:$NODE_VERSION" \
+    npm install
+fi
+
+echo "running $name > $project > $path on container node:$NODE_VERSION"
+
 docker run \
   --rm \
+  --init \
   $PORTS \
   --name "$name" \
   -v "$path":"/app/$project" \
+  $VOLUMES \
   -w "/app/$project" \
   -it "node:$NODE_VERSION" \
   $RUN
