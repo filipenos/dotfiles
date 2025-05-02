26b0cba608517c4dc7b0f0a564f1e9b11f492f42
Filipe Peixoto <filipenos@gmail.com>
2025-05-02 10:48:41 -0300

    feat: option to show logs

--- containers/run-open-webui.sh
+++ containers/run-open-webui.sh
@@ -3,11 +3,14 @@
 NAME=run-open-webui
 
 if [ "$(docker ps -q -f name=$NAME)" ]; then
-  read -p "alread running container $NAME, stop this (yes|no): " yesno
+  read -p "alread running container $NAME, stop this (yes|no|logs): " yesno
   if [ "$yesno" = "yes" ]; then
     echo "stop container"
     docker stop $NAME
     docker rm -f $NAME
+  elif [ "$yesno" = "logs" ]; then
+    echo "showing logs for container $NAME"
+    docker logs --follow $NAME
   fi
   exit 0
 fi
