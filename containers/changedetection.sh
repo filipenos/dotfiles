b0670b2654b4d1c7013a75b38805295b5e453541
Filipe Peixoto <filipenos@gmail.com>
2026-02-24 14:11:41 -0300

    chore(containers,installers): update container scripts and font installer

--- containers/changedetection.sh
+++ containers/changedetection.sh
@@ -18,7 +18,7 @@ fi
 docker run \
   -d \
   --restart always \
-  -p "127.0.0.1:5000:5000" \
+  -p "5000:5000" \
   -v changedetection:/datastore \
   --name $NAME \
   dgtlmoon/changedetection.io
--- containers/run-portainer.sh
+++ containers/run-portainer.sh
@@ -15,7 +15,8 @@ docker run \
   -d \
   -p 9443:9443 \
   --name $NAME \
-  --restart=always \
+  --restart always \
+  --pull always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_data:/data \
   portainer/portainer-ce:latest
